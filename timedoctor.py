2058de8929eb73335bc1fd88869c7355a64f679a
lavedon <lavedon@gmail.com>
2017-10-31 12:24:53 -0400

    Displays usage for month in hours:minutes:seconds.  Set some goals for what the app should do.

--- timedoctor.py
+++ timedoctor.py
@@ -44,4 +44,26 @@ myCompleteApiCall = timeURL + requestURL
 
 myData = urllib.request.urlopen(myCompleteApiCall).read().decode('utf-8')
 myDataJson = json.loads(myData)
-print(myDataJson)
+
+worked = myDataJson.get("worklogs").get("items")
+
+print("This month:")
+for i in worked:
+    seconds = int(i.get("length"))
+    m, s = divmod(seconds, 60)
+    h, m = divmod(m, 60)
+    duration = "%d:%02d:%02d" % (h, m, s)
+    print("{}: {}".format(i.get("task_name"), duration))
+    
+#  @TODO similar print out for projects only
+#  @TODO display calendar of total time each day in the month
+#  @TODO Average last 5 days
+#  @TODO Average last 7 days 
+#  @TODO Max total time 'best time' last month
+#  @TODO Max total time 'best total time worked' this week
+#  @TODO some neat graphs
+#  @TODO GUI with PyQt
+#  @TODO Efficiency Ratio stuff - Start and End times / Time Doctor Time
+#  @TODO More efficiency ratio - so far today - best of the week - best of the month
+#  @TODO Best way is to download data at the beginning for this month, last month, last week, today, last 5 days
+
